9c2b21155e50f66c4bb8012e03846e02730439c1
Jonathan Ruiz <jonathan.rs97@hotmail.com>
2023-08-23 22:37:01 -0500

    feat: crear función de busqueda en tabla de clientes

--- src/app/dashboard/clients/Table.tsx
+++ src/app/dashboard/clients/Table.tsx
@@ -1,16 +1,30 @@
-
 import React from "react";
 import {
+  FilterFn,
   createColumnHelper,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+
+import {
+  RankingInfo,
+  rankItem,
+} from "@tanstack/match-sorter-utils";
+
+declare module "@tanstack/table-core" {
+  interface FilterFns {
+    fuzzy: FilterFn<unknown>;
+  }
+  interface FilterMeta {
+    itemRank: RankingInfo;
+  }
+}
 
 import { ClientTable } from "../../../../types/Client";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faPhone } from "@fortawesome/free-solid-svg-icons";
-
+import { faHome, faPhone, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const columnHelper = createColumnHelper<ClientTable>();
 
@@ -38,65 +52,112 @@ const columns = [
   }),
 ];
 
-function ClientTable( {data}: {data: ClientTable[]}) {
-  
+const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+  // Obtener el rango del item
+  const itemRank = rankItem(row.getValue(columnId), value);
+
+  // Almacenar la información de rango del item
+  addMeta({
+    itemRank,
+  });
+
+  // Retornar si el item debe ser filtrado
+  return itemRank.passed;
+};
+
+function ClientTable({ data }: { data: ClientTable[] }) {
+  /**
+   * Implementación de búsqueda
+   */
+  const [searchQuery, setSearchQuery] = React.useState("");
   const table = useReactTable({
     data,
     columns,
+    filterFns: {
+      fuzzy: fuzzyFilter,
+    },
+    state: {
+      globalFilter: searchQuery,
+    },
+    onGlobalFilterChange: setSearchQuery,
+    globalFilterFn: fuzzyFilter,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
   });
 
   return (
-    <table className="items-center w-full border-collapse">
-      <thead className="text-sm">
-        <tr>
-          <th>Nombre</th>
-          <th className="hidden md:table-cell">Dirección</th>
-          <th>Status</th>
-          <th> </th>
-        </tr>
-      </thead>
-      <tbody className="text-xs">
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            <td className="p-2 md:px-4 h-[68px] md:h-[52px]">
-              <span className="block mb-1 font-medium">
-                {row.original.clientName}
-              </span>
-              <span className="block md:hidden">
-                <FontAwesomeIcon icon={faHome} className="mr-2 text-slate-500" size="xs" />
-                {row.original.fullAddress}
-              </span>
-              {
-                // si tiene teléfono, mostrarlo
-                row.original.phone && (
-                  <a
-                    href={`tel:${row.original.phone}`}
-                    className="text-slate-500 hover:text-slate-700"
-                  >
-                    <FontAwesomeIcon
-                      icon={faPhone}
-                      className="mr-2"
-                      size="xs"
-                    />
-                    {row.original.phone}
-                  </a>
-                )
-              }
-            </td>
-            <td className="hidden md:table-cell md:px-4">
-              {row.original.fullAddress}
-            </td>
-            <td className="p-2 md:px-4">
-              <div className="flex justify-center">
-                <StatusBadgeButton status={row.original.status} />
-              </div>
-            </td>
-            <td className="p-2 md:px-4">...</td>
+    <div>
+      <div className="flex relative ">
+        <div className="absolute h-full w-8 flex items-center justify-center">
+          <FontAwesomeIcon
+            icon={faSearch}
+            className="text-slate-500 m-0 "
+            size="sm"
+          />
+        </div>
+        <input
+          type="text"
+          placeholder="Buscar..."
+          value={searchQuery ?? ""}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className=" bg-gray-100 p-2 pl-8 rounded-md w-full md:w-auto"
+        />
+      </div>
+      <table className="items-center w-full border-collapse">
+        <thead className="text-sm">
+          <tr>
+            <th>Nombre</th>
+            <th className="hidden md:table-cell">Dirección</th>
+            <th>Status</th>
+            <th> </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="text-xs">
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              <td className="p-2 md:px-4 h-[68px] md:h-[52px]">
+                <span className="block mb-1 font-medium">
+                  {row.original.clientName}
+                </span>
+                <span className="block md:hidden">
+                  <FontAwesomeIcon
+                    icon={faHome}
+                    className="mr-2 text-slate-500"
+                    size="xs"
+                  />
+                  {row.original.fullAddress}
+                </span>
+                {
+                  // si tiene teléfono, mostrarlo
+                  row.original.phone && (
+                    <a
+                      href={`tel:${row.original.phone}`}
+                      className="text-slate-500 hover:text-slate-700"
+                    >
+                      <FontAwesomeIcon
+                        icon={faPhone}
+                        className="mr-2"
+                        size="xs"
+                      />
+                      {row.original.phone}
+                    </a>
+                  )
+                }
+              </td>
+              <td className="hidden md:table-cell md:px-4">
+                {row.original.fullAddress}
+              </td>
+              <td className="p-2 md:px-4">
+                <div className="flex justify-center">
+                  <StatusBadgeButton status={row.original.status} />
+                </div>
+              </td>
+              <td className="p-2 md:px-4">...</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
